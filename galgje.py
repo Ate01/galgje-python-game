459a999ba3b7bb36941dbf6630a168afe2505085
Ate01 <45490743+Ate01@users.noreply.github.com>
2019-06-28 18:52:50 +0200

    galgje nodig gemaakt

--- galgje.py
+++ galgje.py
@@ -1,52 +1,53 @@
 import random
 def spel():
+
   turns = 10
 
     
   def drawgalgjes():
     if turns == 9:
         print("""  
-        |
-        |
-        |
-        |
-        |
+         |
+         |
+         |
+         |
+         |
     _____|""")
 
     if turns == 8:
         print("""  ____
-        |
-        |
-        |
-        |
-        |
+         |
+         |
+         |
+         |
+         |
     _____|""")
 
     if turns == 7:
         print("""  ____
         \|
-        |
-        |
-        |
-        |
+         |
+         |
+         |
+         |
     _____|""")
 
     if turns == 6:
         print("""  ____
       | \|
-        |
-        |
-        |
-        |
+         |
+         |
+         |
+         |
     _____|""")
 
     if turns == 5:
         print("""  ____
       | \|
       o  |
-        |
-        |
-        |
+         |
+         |
+         |
     _____|""")
 
     if turns == 4:
@@ -54,32 +55,33 @@ def spel():
       | \|
       o  |
       |  |
-        |
-        |
+         |
+         |
     _____|""")
 
     if turns == 3:
         print("""  ____
       | \|
       o  |
-    /|\  |
-        |
-        |
+     /|\ |
+         |
+         |
     _____|""")
 
     if turns == 2:
         print("""  ____
       | \|
       o  |
-    /|\ |
-    / \ |
-        |
+     /|\ |
+     / \ |
+         |
     _____|""")
 
     if turns == 1:
 
         print("Je hebt het woord", hetwoord, "niet kunnen raden, volgende keer beter!")
-        exit();
+        print()
+        spel();
     
 
 
@@ -90,10 +92,10 @@ def spel():
 
   hetwoord= random.choice(woordenlijst)
   lengtewoord = len(hetwoord)
-  puntjes = ["_ "] * lengtewoord
+  puntjes = [" ~ "] *lengtewoord
 
 
-  naam = input("vul hier je naam in: ")
+  naam = input("Vul hier je naam in: ")
   print()
   print("Welkom", naam,"bij ons galgje spel")
   game = True
@@ -101,15 +103,16 @@ def spel():
   print("Raad de letters en als je het woord weet typ het dan in.")
   print()
   print("het woord heeft " + str(lengtewoord) + " letters")
-  print(puntjes)
+ 
 
 
 
   while game == True:
-    userGuess = input("geef letter> ")
+    userGuess = input("geef letter/woord> ")
     #zit letter in woord? Zo ja, waar dan?
     if userGuess == hetwoord:
-      print ("gewonnen")
+      print ("gefeliciteerd je hebt het woord", hetwoord, "geraden")
+      print()
       spel()
     else:
       if userGuess in hetwoord:
@@ -117,7 +120,7 @@ def spel():
           
           if(letter == userGuess):
             puntjes[idx] = userGuess
-
+      
       #zo niet, beurt eraf en galgjes tekenen
       else:
         turns -= 1
@@ -126,25 +129,12 @@ def spel():
     print(''.join(puntjes))
 
 
-
-  guessed = ""
-
-  missed = 1
-
-
+ 
   if puntjes == hetwoord:
       print(hetwoord)
       print("je hebt het woord", hetwoord, "geraden")
 
   gokje = input()
 
-
-  if gokje not in letters:
-    print("vul een geldig woord in")
-
-    gokje = input()
-
-    if guess not in word:
-      turns = turns - 1
     
 spel()
